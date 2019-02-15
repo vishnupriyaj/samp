1d094443c4482614a44ff5a589581aeb4b761f7e
jagdish <jagdish@jagdish-PC>
2019-02-15 09:01:13 +0530

    changes are made

--- ghgh/src/ghgh/Fibo.java
+++ ghgh/src/ghgh/Fibo.java
@@ -3,6 +3,7 @@ package ghgh;
 public class Fibo {
 	public static void main(String[]args) {
 		int n1=0,n2=0,n3=1;
+		int k=20,j=30;
 		for(int i=0;i<=10;i++){
 			System.out.println(n2);
 			n1=n2;
